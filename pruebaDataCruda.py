96d6076060c75bd6e62c48332ccf22b62697b016
julian <julianeduardovillamizarpena_14021994@outlook.com>
2022-07-14 17:55:45 -0500

    Generate new Data

--- pruebaDataCruda.py
+++ pruebaDataCruda.py
@@ -1,44 +1,64 @@
 import pandas as pd
 import random as rd
 import numpy as np
+from decimal import *
 
 column_names = ["Accx","Accy","Accz","Apogeo1","Apogeo2","Tiempo",
              "Altura","Presion","GPSlat","GPSlong","Power","Fases","temp","girox","giroy","giroz"]
 
-generador = np.zeros((1000,16))
+#generador = np.zeros((1000,16))
 
-for i in range(generador.shape[0]):
-    for j in range(generador.shape[1]):
-        if i == 0 or i== 1 or i== 2:
-            generador[i][j] = round(rd.uniform(0,99), 4)    
-        elif i== 3 or i==4:
-            generador[i][j] = round(rd.uniform(0,1))  
-        elif i== 5:
-            generador[i][j] = round(rd.uniform(0,3000000))
-        elif i== 6:
-            generador[i][j] = round(rd.uniform(0,500000),2)
-        elif i== 7:
-            generador[i][j] = round(rd.uniform(0,1),9)
-        elif i== 8 or i==9:
-            generador[i][j] = round(rd.uniform(0,180),6)
-        elif i== 10:
-            generador[i][j] = round(rd.uniform(0,1))
-        elif i== 11:
-            generador[i][j] = round(rd.uniform(0,6))
-        elif i== 12:
-            generador[i][j] = round(rd.uniform(-40,90),3)
-        elif i== 13:
-            generador[i][j] = round(rd.uniform(0,180),6)
-        elif i== 14 or i== 15 or i==16:
-            generador[i][j] = round(rd.uniform(0,360),2)
-        else:
-            pass
+Accx = [0.0] *10
+Accy = [0.0] *10 
+Accz = [0.0] *10 
+Apogeo1 = [0.0] * 10 
+Apogeo2 = [0.0] * 10
+Tiempo = [0.0] * 10 
+Altura = [0.0] * 10 
+Presion = [0.0] * 10
+GPSlat = [0.0] * 10
+GPSlong = [0.0] * 10  
+Power = [0.0] * 10 
+Fases = [0.0] *10
+temp = [0.0] * 10
+girox = [0.0] * 10
+giroy = [0.0] * 10
+giroz = [0.0] * 10
 
 
-##Imprimir el array de información
-for i in range(generador.shape[0]):
-    for j in range(generador.shape[1]):
-        print(generador[i][j], end = " ")
-    print("\n")
+
+#getcontext().prec = 6
+presionTmp = 101326
+
+for i in range(1,10):
+    #"Accx","Accy","Accz"
+    Accx[i], Accy[i], Accz[i] = round(rd.uniform(0,99), 4)    
+    #"Apogeo1","Apogeo2"
+    Apogeo1[i], Apogeo2[i] = rd.uniform(0,1) 
+    #"Tiempo"
+    Tiempo[i] = round(rd.uniform(0,1800))
+    #"Altura"
+    Altura[i] = round(rd.uniform(0,5000),2)
+    #"Presion"
+    presionTmp -= 133.322
+    Presion[i] = presionTmp 
+    #"GPSlat", "GPSlong"
+    GPSlat[i], GPSlong[i] = round(rd.uniform(0,180),6)
+    #"Power"
+    Power[i] = round(rd.uniform(0,1))
+    #"Fases"
+    Fases[i] = round(rd.uniform(0,6))
+    #"temp"[Celsius]
+    temp[i] = round(rd.uniform(-40,30),3)
+    #"girox", "giroy", "giroz"
+    girox[i], giroy[i], giroz[i] = round(rd.uniform(0,360),2)
+    
+
+df = pd.DataFrame(list(zip(Accx, Accy, Accz, Apogeo1, Apogeo2, \
+    Tiempo, Altura, Presion, GPSlat, GPSlong, Power, Fases, \
+    temp, girox, giroy, giroz)), \
+    columns = column_names)
+
+df
 
     
